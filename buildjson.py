d025ca0b18eacdbd9289cb74b11c41fe348a3d65
Oliver12418 <106941247+Oliver12418@users.noreply.github.com>
2026-02-15 01:35:44 +0000

    did some stuff I think
    also i got an autosuggestion saying "are we allowed
    to ask for credit? i don't think so?" soo i think
    that's fun i guess. also im getting SO MANY
    suggestions on this, like, a lot. so that's fun.

--- buildjson.py
+++ buildjson.py
@@ -1,13 +1,11 @@
 import os
-list = os.listdir("./games/")
-json = open("./games.json", "w")
-json.write('')
-json.write(
-    str(list)
-    .replace('[', '[\n ')
-    .replace(']', '\n]')
-    .replace(',', ',\n')
-    .replace('\'', '\"')
-    .replace(' ', '    ')
-    .replace('.html', '')
+import json
+
+lst = sorted(
+    f for f in os.listdir("./games/")
+    if f.endswith(".html") and os.path.isfile(os.path.join("games", f))
 )
+files = [os.path.splitext(f)[0] for f in lst]
+
+with open("./games.json", "w", encoding="utf-8") as file:
+    json.dump(files, file, indent=4, ensure_ascii=False)
